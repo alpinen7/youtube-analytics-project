05a892d2d94c6cb4ac7742fb30869d76b62084c1
Alexey Lebedev <lazdanny@mail.ru>
2023-11-25 13:03:13 +0300

    реализована логика программы ДЗ_1 ООП

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
-
+import json
+import os
+from googleapiclient.discovery import build
+import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = "AIzaSyCmOygzhlZwoIzxmKzigRrFcJhE3Z3srhQ"
+    # api_key = os.getenv('YT_API_KEY'))
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        if type(channel_id) is not str:
+            raise ValueError("Значение должно быть типа строка")
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel_info = print(json.dumps(channel, indent=2, ensure_ascii=False))
+        return channel_info
